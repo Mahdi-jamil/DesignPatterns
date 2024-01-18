653777357700d2a87e5d39acbb643016c9cc39e7
Mahdi-jamil <jamilmahdi77@gmail.com>
2024-01-18 17:06:47 +0300

    Add MVC

--- src/main/java/com/devesta/i5course/javaFX/lab/ex1/HelloApplication.java
+++ src/main/java/com/devesta/i5course/javaFX/lab/ex1/HelloApplication.java
@@ -33,8 +33,8 @@ public class HelloApplication extends Application {
             try {
                 String input = textField.getText();
                 float enteredNumber = Float.parseFloat(input);
-                float fahrenheit = controller.toFahrenheit(enteredNumber);
-                result.setText("result : " + fahrenheit);
+                float Fahrenheit = controller.toFahrenheit(enteredNumber);
+                result.setText("result : " + Fahrenheit);
             } catch (Exception e) {
                 result.setText("invalid input");
             }
